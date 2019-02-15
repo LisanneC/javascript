9f71d21b334a7c40d834abdbdbc775f76dd9c730
Lisanne Chin <lisanne.chin@gmail.com>
2019-02-15 16:33:47 +0100

    Create child array

--- scripts/modules/parent.js
+++ scripts/modules/parent.js
@@ -1,5 +1,18 @@
 export default class Parent {
   constructor(){
-    console.log('parent');
+    
+    this.child = [];
+
+    let i = 0;
+    for (i; i < 5; i++) {
+      this.child.push(new Child());
+    };
+
+    console.log(this.child);
   }
 };
+
+class Child {
+  constructor() {
+  }
+}
